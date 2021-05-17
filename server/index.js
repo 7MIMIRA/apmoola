bb7a491761aa544b7c5a463355108eaf447d7275
7MIMIRA <7mimira@gmail.com>
2021-05-17 01:31:39 -0400

    Implement GraphQL API with Apollo Server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('../schema/schema.js');
+const resolvers = require('../resolvers/resolvers.js');
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen()
+  .then(({ url }) => {
+    console.log(`Server listening on ${url}`);
+  });
