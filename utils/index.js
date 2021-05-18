9bd9519f207a5babb15d07fb92fa620fe77bc5ce
7MIMIRA <7mimira@gmail.com>
2021-05-18 01:10:20 -0400

    Add comment

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // arguments must be in milliseconds, returns true if event falls within window start and end times inclusive
   isWithinTimeWindow: (windowStart, windowEnd, eventStart, eventEnd) => {
     return windowStart <= eventStart && windowEnd >= eventEnd;
   }
