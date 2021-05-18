60049d0d6e5ef4c3875ca0fb66739dbc5ecd180e
7MIMIRA <7mimira@gmail.com>
2021-05-18 00:43:31 -0400

    Add comments and denote required fields

--- schema/schema.js
+++ schema/schema.js
@@ -2,37 +2,55 @@ const { gql } = require('apollo-server');
 
 module.exports = gql`
   type App {
-    id: ID
-    name: String
-    events(start: Int, end: Int): [Event]
-    stages: [Stage]
+    id: ID!
+    name: String!
+    """
+    start and end parameters are optional if used, it will limit results to events that
+    fall within the start and end dates (inclusive)
+
+    Required Format: milliseconds since unix epoch
+    """
+    events(start: Int, end: Int): [Event!]!
+    stages: [Stage!]!
   }
 
   type Stage {
-    id: ID
-    name: String
-    events(start: Int, end: Int): [Event]
+    id: ID!
+    name: String!
+    """
+    start and end parameters are optional if used, it will limit results to events that
+    fall within the start and end dates (inclusive)
+
+    Required Format: milliseconds since unix epoch
+    """
+    events(start: Int, end: Int): [Event!]!
   }
 
   type Event {
-    id: ID
-    appId: ID
-    stageId: ID
-    name: String
-    description: String
-    image: String
-    startsAt: Int
-    endsAt: Int
-    stage: Stage
+    id: ID!
+    appId: ID!
+    stageId: ID!
+    name: String!
+    description: String!
+    image: String!
+    startsAt: Int!
+    endsAt: Int!
+    stage: Stage!
   }
 
   type Query {
-    apps: [App]
-    stages: [Stage]
-    events(start: Int, end: Int): [Event]
+    apps: [App!]!
+    stages: [Stage!]!
+    """
+    start and end parameters are optional if used, it will limit results to events that
+    fall within the start and end dates (inclusive)
 
-    app(name: String): App
-    stage(name: String): Stage
-    event(name: String): Event
+    Required Format: milliseconds since unix epoch
+    """
+    events(start: Int, end: Int): [Event!]!
+
+    app(name: String!): App!
+    stage(name: String!): Stage!
+    event(name: String!): Event!
   }
 `;
