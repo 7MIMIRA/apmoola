be2a472a379b7b359b4c8fc41abffa8ac697e5c8
7MIMIRA <7mimira@gmail.com>
2021-05-18 23:16:13 -0400

    Implement generateID function
    When "add" mutations are used, generateID will be used to create a unique
    id value for the new record being created.

--- utils/index.js
+++ utils/index.js
@@ -2,5 +2,13 @@ module.exports = {
   // arguments must be in milliseconds, returns true if event falls within window start and end times inclusive
   isWithinTimeWindow: (windowStart, windowEnd, eventStart, eventEnd) => {
     return windowStart <= eventStart && windowEnd >= eventEnd;
+  },
+
+  generateID: (value) => {
+     return Buffer.from(value + randomNumberGenerator()).toString('base64');
   }
+}
+
+function randomNumberGenerator() {
+  return Math.floor(Math.random() * 10000000);
 }
