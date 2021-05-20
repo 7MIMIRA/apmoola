9885a86b21edb76d4dbf301ac06ac8ad92813bd8
7MIMIRA <7mimira@gmail.com>
2021-05-19 22:48:21 -0400

    Fix apollo server w/ express

--- server/server.js
+++ server/server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('../schema/schema.js');
 const resolvers = require('../resolvers/resolvers.js');
-const server = new ApolloServer({ typeDefs, resolvers });
-const app = express();
 
-server.applyMiddleware({ app });
-app.listen({ port: 4000 }, () =>
-  console.log('Now browse to http://localhost:4000' + server.graphqlPath)
-);
+startApolloServer();
+
+async function startApolloServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  await new Promise(resolve => app.listen({ port: 4000 }, resolve));
+  console.log(`listening on http://localhost:4000${server.graphqlPath}`);
+  return { server, app };
+}
