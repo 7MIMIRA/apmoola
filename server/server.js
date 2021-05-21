84651a545746c99f83a3c0b737a55196144dc8e2
7MIMIRA <7mimira@gmail.com>
2021-05-21 01:33:53 -0400

    Upgrade to leet port value

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const typeDefs = require('../schema/graphql.js');
 const resolvers = require('../resolvers/resolvers.js');
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen()
+server.listen({ port: 1337 })
   .then(({ url }) => {
     console.log(`Server listening on ${url}`);
   });
